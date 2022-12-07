43204e5e94b213cf3af71bfa7f8399d180ebf2ab
warrenhanwen <warren_hanwen@163.com>
2022-12-06 20:29:06 -0800

    linked opencv lib dir to lib search dir.s

--- localization/cmake/OpenCV.cmake
+++ localization/cmake/OpenCV.cmake
@@ -1,6 +1,7 @@
 find_package(OpenCV 3.2 REQUIRED)
 
 include_directories(${OpenCV_INCLUDE_DIRS})
+link_directories("/usr/local/lib/")
 list(APPEND ALL_TARGET_LIBRARIES ${OpenCV_LIBS})
 MESSAGE("OpenCV_LIBS:")
 MESSAGE(${OpenCV_LIBS})
